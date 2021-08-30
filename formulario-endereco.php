5c17562a10a847632ccb99e80a1d1fb0f16515d6
Helison Santos Cruz <helison@agenciahouse.com.br>
2021-08-30 14:57:47 -0300

    Adicionando modelo Formulário Endereço

---
+++ formulario-endereco.php
@@ -0,0 +1,50 @@
+<div class="formulario">
+    <div class="row">
+        <div class="col-3">
+            <div class="mb-4 form">
+                <input type="text" name="cep" class="form-input cep cep-field" placeholder=" ">
+                <label class="form-label">CEP</label>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="mb-4 form">
+                <input type="text" name="endereco" class="form-input endereco" placeholder=" ">
+                <label class="form-label">Endereço</label>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="mb-4 form">
+                <input type="text" name="numero" class="form-input" placeholder=" ">
+                <label class="form-label">Número</label>
+            </div>
+        </div>
+    </div>
+    <div class="row gx-4">
+        <div class="col-4">
+            <div class="mb-4 form">
+                <input type="text" name="complemento" class="form-input" placeholder=" ">
+                <label class="form-label">Complemento</label>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="mb-4 form">
+                <input type="text" name="bairro" class="form-input bairro" placeholder=" ">
+                <label class="form-label">Bairro</label>
+            </div>
+        </div>
+    </div>
+    <div class="row gx-4">
+        <div class="col-3">
+            <div class="mb-4 form">
+                <input type="text" name="estado" class="form-input estado" placeholder=" ">
+                <label class="form-label">Estado</label>
+            </div>
+        </div>
+        <div class="col-5">
+            <div class="mb-4 form">
+                <input type="text" name="cidade" class="form-input cidade" id="cr-input-10" placeholder=" ">
+                <label class="form-label">Cidade</label>
+            </div>
+        </div>
+    </div>
+</div>
